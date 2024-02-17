4c502049e3a43e10886eb4cd15744bcb0ed52ca6
NDalbec <nicolas.dalbec-constant@polymtl.catl.ca>
2024-02-17 17:12:26 -0500

    1d GMM

--- src/AutoencoderAPI/utils/clustering/poissonGaussianMixture.py
+++ src/AutoencoderAPI/utils/clustering/poissonGaussianMixture.py
@@ -1,62 +1,143 @@
 import numpy as np
-from scipy.stats import multivariate_normal
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+from tqdm.notebook import tqdm
 
 
 class GMM_1D:
-    def __init__(self, k, max_iter=5):
-        self.k = k
-        self.max_iter = int(max_iter)
+    """
+    
 
-        self.mu = None # Cluster mean
-        self.sigma = None # Cluster variance
+    Parameters
+    ----------
+    n_cluster : int
+        The number of clusters
+    pi : array-like, shape (n_cluster,)
+        The weight of each mixture component.
+    mu : array-like, shape (n_cluster,)
+        The means of each mixture component.
+    sigma : array-like, shape (n_cluster,)
+        The standard deviation of each mixture component.
+    gamma :
+        Posterior probability that component k was responsible for generating sample x_n
+    """
+    def __init__(self, n_cluster = 3, 
+                        n_step = 3,
+                        tol = 400):
+        
+        self.n_cluster = n_cluster
+        self.n_sample = None
+        self.n_step = n_step
+        self.tol = tol
+        self.pi = None
+        self.mu = None
+        self.sigma = None
+        self.gamma = None
+        self.likelihood = []
+
 
     def initialize(self, X):
-        #self.shape = X.shape
-        #self.n, self.m = self.shape
-        self.n = len(X) # Number of samples
-
-        self.pi = np.full(shape=self.k, fill_value=1/self.k)
-        self.weights = np.full( shape=self.k, fill_value=1/self.k)
+        """
+        Initialize means, weights and variance randomly
         
-        random_row = np.random.randint(low=0, high=self.n, size=self.k)
-        self.mu = [  X[row_index,:] for row_index in random_row ]
-        self.sigma = [ np.cov(X) for _ in range(self.k) ]
+        """
+        self.pi = np.ones(self.n_cluster) / self.n_cluster
+        self.mu = np.random.choice(X, self.n_cluster)
+        self.sigma = np.random.random_sample(size = self.n_cluster)
 
-    def e_step(self, X):
-        # E-Step: update weights and pi holding mu and sigma constant
-        self.weights = self.predict_proba(X)
-        self.pi = self.weights.mean(axis=0)
-    
-    def m_step(self, X):
-        # M-Step: update mu and sigma holding pi and weights constant
-        for i in range(self.k):
-            weight = self.weights[:, [i]]
-            total_weight = weight.sum()
-            self.mu[i] = (X * weight).sum(axis=0) / total_weight
-            self.sigma[i] = np.cov(X.T, 
-                aweights=(weight/total_weight).flatten(), 
-                bias=True)
+        self.get_likelihood(X)
+
+
+    def E_step(self, X):
+        """
+        E Step
+        
+        Parameters
+        ----------
+        """
+        product = np.zeros((self.n_sample, self.n_cluster))
+        for index_cluster in range(self.n_cluster):
+            normal = norm.pdf(x = X, 
+                            loc = self.mu[index_cluster], 
+                            scale = self.sigma[index_cluster])
+            product[:,index_cluster] = self.pi[index_cluster] * normal
+        
+        self.gamma = product / np.sum(product, axis=0)
+
+
+    def M_step(self, X):
+        """M Step
+        
+        Parameters
+        ----------
+            
+        Returns
+        -------
+        """
+        X = X.reshape(-1,1)
+        N_k = np.sum(self.gamma, axis = 0)
+        
+
+        self.mu = np.sum(self.gamma * X, axis = 0) / N_k
+        self.sigma = np.sum(self.gamma * (X - self.mu) ** 2, axis = 0) / N_k
+        self.pi = N_k / self.n_sample
+
+
+    def get_likelihood(self, X):
+        """
+        
+        """
+        product = np.zeros((self.n_cluster, self.n_sample))
+
+        for index_cluster in range(self.n_cluster):
+            normal = norm.pdf(x = X, 
+                            loc = self.mu[index_cluster], 
+                            scale = self.sigma[index_cluster])
+            product[index_cluster,:] = self.pi[index_cluster] * normal
+
+        self.likelihood.append(np.sum(np.log(np.sum(product, axis = 0)), axis = 0))
+
 
     def fit(self, X):
-        self.initialize(X)
+        """ Training step of the GMM model
         
-        for iteration in range(self.max_iter):
-            self.e_step(X)
-            self.m_step(X)
-            
-    def predict_proba(self, X):
-        likelihood = np.zeros( (self.n, self.k) )
-        for i in range(self.k):
-            distribution = multivariate_normal(
-                                        mean=self.mu[i], 
-                                        cov=self.sigma[i])
-            likelihood[:,i] = distribution.pdf(X)
+        Parameters
+        ----------
+        data : array-like, shape (n_samples,)
+            The data.
+        n_components : int
+            The number of clusters
+        n_steps: int
+            number of iterations to run
+        """
+        X = X.reshape(1,-1)
+        self.n_sample = X.shape[1]
+        self.initialize(self.n_cluster)
+
+        for step in tqdm(range(self.n_step), total=self.n_step):
+            self.E_step(X)
+            self.M_step(X)
+            self.get_likelihood(X)
+
+            #if self.likelihood < self.tol:
+            #    break
         
-        numerator = likelihood * self.pi
-        denominator = numerator.sum(axis=1)[:, np.newaxis]
-        weights = numerator / denominator
-        return weights
-    
-    def predict(self, X):
-        weights = self.predict_proba(X)
-        return np.argmax(weights, axis=1)
+        plt.figure()
+        #plt.hist(X, bins=1000)
+        lin = np.arange(-2,1,0.01)
+        for index_cluster in range(self.n_cluster):
+            normal = norm.pdf(x = lin, 
+                            loc = self.mu[index_cluster], 
+                            scale = self.sigma[index_cluster])
+            plt.plot(lin, normal)
+        plt.show()
+
+        plt.figure()
+        plt.plot(self.likelihood)
+        plt.show()
+
+
+
+
+
+
